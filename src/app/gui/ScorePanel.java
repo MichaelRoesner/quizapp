b48f68599948f5d21b82e5ed31c205ff3642ce4a
Michael Rösner <mmcoolj@gmx.net>
2023-11-27 18:03:03 +0100

    Final V3.2 - added commentary for ScorePanel.java

--- src/app/gui/ScorePanel.java
+++ src/app/gui/ScorePanel.java
@@ -14,7 +14,10 @@ import java.util.Map;
 public class ScorePanel extends JPanel {
 
     /**
-     * Create the panel.
+     * Constructor for ScorePanel.
+     * @param parentFrame The main frame of the application.
+     * @param answeredQuestion A map containing questions and the corresponding user-selected answers.
+     * @param score The user's score.
      */
     public ScorePanel(MainFrame parentFrame, Map<Question, Answer> answeredQuestion, int score) {
 
@@ -27,18 +30,21 @@ public class ScorePanel extends JPanel {
         closeButton.setBounds(654, 6, size.width + 10, size.height + 10);
         add(closeButton);
 
+        // Title Label
         StrokedLabel lblNewLabel = new StrokedLabel("Quiz Result", new Color(255, 255, 255), 2, ResourcePath.PIXEL, 40f);
         lblNewLabel.setText("Quiz Results");
         //lblNewLabel.setForeground(new Color(0, 0, 0));
         lblNewLabel.setBounds(10, 6, 153, 34);
         add(lblNewLabel);
 
+        // Score Label
         JLabel lblNewLabel_1 = new JLabel("Score");
         lblNewLabel_1.setForeground(new Color(255, 255, 255));
         lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 18));
         lblNewLabel_1.setBounds(605, 44, 64, 27);
         add(lblNewLabel_1);
 
+        // Display the user's score.
         JLabel scoreLabel = new JLabel(score + "/" + answeredQuestion.size());
         scoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
         scoreLabel.setForeground(new Color(128, 255, 255));
@@ -46,12 +52,13 @@ public class ScorePanel extends JPanel {
         scoreLabel.setBounds(605, 70, 64, 22);
         add(scoreLabel);
 
+        // Container Panel for individual question results
         JPanel containerPanel = new JPanel();
         containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
 
         int questionNumber = 1;
-        // Add individual panels to the container panel
 
+        // Add individual panels to the container panel
         for(Question question: answeredQuestion.keySet())
         {
             String questionText = question.getQuestion();
@@ -61,17 +68,19 @@ public class ScorePanel extends JPanel {
             String selectedAnswer = "No Option Selected";
             if(answer!=null)
                 selectedAnswer = answer.getText();
+            // Create an AnsweredQuestionPanel for each question.
             AnsweredQuestionPanel panel = new AnsweredQuestionPanel(questionNumber, questionText, selectedAnswer, correctAnswer);
             containerPanel.add(panel);
             questionNumber++;
         }
 
+        // ScrollPane for the container panel
         JScrollPane scrollPane = new JScrollPane(containerPanel);
         scrollPane.setBounds(10, 110, 680, 437);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
         add(scrollPane);
 
+        // Play sound based on the user's score
         if (score == answeredQuestion.size()) {
             SoundPlayer.playSound(ResourcePath.SOUND_WIN_PATH);
         } else {
@@ -80,6 +89,10 @@ public class ScorePanel extends JPanel {
     }
 
 
+    /**
+     * Override the getPreferredSize method to specify the preferred size of the panel.
+     * @return The preferred size of the panel.
+     */
     @Override
     public Dimension getPreferredSize() {
         // Return the preferred size of the panel
